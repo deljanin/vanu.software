42167c5f367a167e9a36a90e100304139f4ba367
Petar <pecadeljanin@gmail.com>
2024-10-16 01:26:27 +0200

    Temporary home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
 export default function Home() {
   return (
-    <div className="font-ron w-full h-screen flex items-center justify-center">
-      <h1 className="text-5xl">Coming soon</h1>
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="font-ron text-5xl">Welcome to Vanu Software</h1>
+      <h2 className="text-xl">
+        This website is under development as I don't have the time for it,
+        because of other projects.
+      </h2>
+      <h2 className="text-xl">
+        You can see some of my current work here:{' '}
+        <a
+          target="_blank"
+          className="font-bold hover:text-purple-600 transition-colors"
+          href="https://apex-smma.com">
+          Apex SMMA,
+        </a>{' '}
+        <a
+          target="_blank"
+          className="font-bold hover:text-purple-600 transition-colors"
+          href="https://www.figma.com/proto/CqCaWUhxSwCOYxwWloCfwJ/FitBar?node-id=0-1&t=gwQ8t8dOdus4o5gI-1">
+          FitBar
+        </a>
+      </h2>
+      <h2 className="text-xl">
+        Reach me by sending an email to:{' '}
+        <a
+          target="_blank"
+          className="font-bold hover:text-purple-600 transition-colors"
+          href="mailto:contact@vanu.software">
+          contact@vanu.software
+        </a>
+      </h2>
     </div>
   );
 }
