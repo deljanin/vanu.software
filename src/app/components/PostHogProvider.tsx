640474eabdeb89c99387e134e29f3ecfd1061dbe
petar <pecadeljanin@gmail.com>
2025-11-08 20:29:14 +0100

    Update posthog analytics to our proxy url.

--- src/app/components/PostHogProvider.tsx
+++ src/app/components/PostHogProvider.tsx
@@ -11,7 +11,7 @@ interface PostHogProviderProps {
 export function PostHogProvider({ children }: PostHogProviderProps) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
+      api_host: "https://analytics.vanu.software",
       ui_host: "https://eu.posthog.com",
       defaults: "2025-05-24",
       capture_exceptions: true,
